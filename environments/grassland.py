2916f54ba33bb3ce4f16e83c8ee5e44a1b1329ff
Tyler Simeone <tyler.simeone@gmail.com>
2020-04-24 09:40:26 -0500

    working on grassland biome

---
+++ environments/grassland.py
@@ -0,0 +1,27 @@
+from interfaces import Identifiable
+from interfaces import IContainsAnimals
+from interfaces import IContainsPlants
+from interfaces import IHigh_Elevation
+from interfaces import ITerrestrial
+
+
+class Grassland(IContainsAnimals, IContainsPlants, Identifiable):
+
+    def __init__(self):
+      IContainsAnimals.__init__(self)
+      IContainsPlants.__init__(self)
+      Identifiable.__init__(self)
+
+    def add_animal(self, animal):
+        try:
+            self.animals.append(animal)
+        except AttributeError:
+            raise AttributeError("Error!")
+
+    def add_plant(self, plant):
+        try:
+            self.plants.append(plant)
+        except AttributeError:
+            raise AttributeError("Error!")
+    def __str__(self):
+        print("grassland object")
