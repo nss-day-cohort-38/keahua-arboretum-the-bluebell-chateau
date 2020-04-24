40a496eebebe31287071d59ca1e64f5000a0c957
Bito Mann <bito@bitomann.com>
2020-04-24 10:44:30 -0500

    costline

---
+++ environments/coastline.py
@@ -0,0 +1,25 @@
+from interfaces import Identifiable
+from interfaces import IContainsAnimals
+from interfaces import IContainsPlants
+
+
+class Coastline(IContainsAnimals, IContainsPlants, Identifiable):
+
+    def __init__(self):
+      IContainsAnimals.__init__(self)
+      IContainsPlants.__init__(self)
+      Identifiable.__init__(self)
+
+    def add_animal(self, animal):
+        try:
+            self.animals.append(animal)
+        except AttributeError:
+            raise AttributeError("Error!")
+
+    def add_plant(self, plant):
+        try:
+            self.plants.append(plant)
+        except AttributeError:
+            raise AttributeError("Error!")
+    def __str__(self):
+        print("coastline object")
